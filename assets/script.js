810cff06ed81ed7a387ff2c11c8b4ca097b371b2
mr.foruk@hotmail.com <mr.forqu@gmail.com>
2023-08-14 15:17:45 +0300

    nem oranı ve 404 eklendi

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,14 @@
 const url = 'https://api.openweathermap.org/data/2.5/'
 const myApiKey = 'c3af7e0d92860c125611293711d1dda5'
 
+let content = document.querySelector(".content");
+
+// humadity divini oluşturur(nem)
+let humadity = document.createElement("div");
+humadity.classList.add("humadity");
+content.appendChild(humadity);
+// console.log(humadity)
+
 
 const setQuery = (event) => {
 
@@ -10,9 +18,14 @@ const setQuery = (event) => {
     }
 }
 
+// enter tuşuna basıldığında setQuery fonksiyonunu çalıştırır
+const searchBox = document.querySelector('#searchBar')
+searchBox.addEventListener('keypress', setQuery)
+
 // şehir adı girildiğinde sonuçları getirir
 const getResult = (cityName) => {
     // istek yapılacak url
+    console.log(cityName)
     let query = `${url}weather?q=${cityName}&appid=${myApiKey}&units=metric&lang=tr`
 
     // fetch ile istek yapılır
@@ -23,25 +36,36 @@ const getResult = (cityName) => {
         .then(displayResult)
 }
 
+// sonuçları ekrana yazdırır
 const displayResult = (result) => {
     console.log(result)
 
-    let city = document.querySelector('.city')
-    city.innerText = `${result.name}, ${result.sys.country}`
+    // şehir bulunamadığında alert verir.
+    if (result.cod == '404') {
+        alert('Şehir bulunamadı. Lütfen geçerli bir şehir adı giriniz. ')
+    } else {
 
-    let temp = document.querySelector('.temp');
-    temp.innerText = `${Math.round(result.main.temp)}°C`
+        let city = document.querySelector('.city')
+        city.innerText = `${result.name}, ${result.sys.country}`
 
-    let description = document.querySelector('.description')
-    description.innerText = `${result.weather[0].description}`
+        let temp = document.querySelector('.temp');
+        temp.innerText = `${Math.round(result.main.temp)}°C`
 
-    let minMaxTemp = document.querySelector('.minMax')
-    minMaxTemp.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
+
+        let description = document.querySelector('.description')
+        description.innerText = `${result.weather[0].description}`
+
+        let minMaxTemp = document.querySelector('.minMax')
+        minMaxTemp.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
+
+        humadity.innerText = ` Nem: ${result.main.humidity}%`
+    }
+
+
 }
 
-// enter tuşuna basıldığında setQuery fonksiyonunu çalıştırır
-const searchBox = document.querySelector('#searchBar')
-searchBox.addEventListener('keypress', setQuery)
+
+
 
 // Sayfa yüklendiğinde varsayılan olarak İstanbul hava durumunu gösterir.s
 window.addEventListener('load', () => {
